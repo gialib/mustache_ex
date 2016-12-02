0deecfe67bec75e2c73180eb9fdaa4f6f5604714
WangJinzhong <andywang7259@163.com>
2016-12-02 10:29:49 +0800

    update version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Mustache.Mixfile do
 
   def project do
     [app: :mustache,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.3.0",
      deps: deps,
      description: description,
